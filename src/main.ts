0c771ab202fae050eed76c97b1fef3e69f2f8cc3
LeonardoBizzoni <leo2002714@gmail.com>
2023-06-21 16:46:45 +0200

    Fixed variable showcase in output table

--- src/main.ts
+++ src/main.ts
@@ -1,35 +1,41 @@
 import { Formula } from "./formula";
+import { Atom } from "./atom";
 
 export function checkFormula() {
-	let input = (document.getElementById("formulaInput") as HTMLInputElement).value;
+    let input = (document.getElementById("formulaInput") as HTMLInputElement).value;
 
-	let formula = new Formula(input.trim());
+    let formula = new Formula(input.trim());
 
-	if (formula.fbf) {
-		(document.getElementById("invalidFormula") as HTMLElement).setAttribute("style", "display: none");
+    if (formula.fbf) {
+	(document.getElementById("invalidFormula") as HTMLElement).setAttribute("style", "display: none");
 
-		let h2 = document.getElementById("treeString") as HTMLElement;
-		h2.setAttribute("style", "display: block");
-		h2.innerHTML = `Syntax tree: </br><b style="padding-left: 20px">${formula.tree}</b>`;
+	let h2 = document.getElementById("treeString") as HTMLElement;
+	h2.setAttribute("style", "display: block");
+	h2.innerHTML = `Syntax tree: </br><b style="padding-left: 20px">${formula.tree}</b>`;
 
-		let table = document.getElementById("formulaTable") as HTMLElement;
-		table.setAttribute("style", "display: block");
-		table.innerHTML = "<h2 class='font-medium leading-tight text-3xl mt-0 mb-2'>Truth table:</h2>" + pprint(formula.evaluate());
-	} else {
-		(document.getElementById("invalidFormula") as HTMLElement).setAttribute("style", "display: block");
-	}
+	let table = document.getElementById("formulaTable") as HTMLElement;
+	table.setAttribute("style", "display: block");
+	table.innerHTML = "<h2 class='font-medium leading-tight text-3xl mt-0 mb-2'>Truth table:</h2>" + pprint(formula.variables, formula.evaluate());
+    } else {
+	(document.getElementById("invalidFormula") as HTMLElement).setAttribute("style", "display: block");
+    }
 }
 
-function pprint(arr: Array<[string, boolean]>): string {
-	let table = "<table class='table table-compact'><thead><tr><th>Assignment</th><th>Value</th></tr></thead><tbody>";
-	for (const [item, value] of arr) {
-		table += `
-            <tr>
-                <td>${item}</td>
-                <td>${value}</td>
-            </tr>
-        `;
+function pprint(vars: Atom[], arr: Array<[string, boolean]>): string {
+    let table = "<table class='table table-compact'><thead><tr>";
+    for (const atom of vars) {
+	table += `<th>${atom.name}</th>`;
+    }
+    table += "<th>Value</th></tr></thead><tbody>";
+
+    for (const [assignment, value] of arr) {
+	table += "<tr>";
+	for (const bit of assignment) {
+	    table += `<td>${bit}</td>`;
 	}
-	table += "</table>";
-	return table;
+	table += `<td>${value}</td>`;
+	table += "</tr>";
+    }
+    table += "</table>";
+    return table;
 }
